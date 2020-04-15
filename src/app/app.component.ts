b0037bb4a80ffa0499ef99a1b356788e8a00cda5
Steve Phuc <stevephuc.dev@gmail.com>
2020-04-15 21:05:02 +0300

    7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learnAngular';
+  onButtonClick() {
+    console.log('Button was clicked');
+
+  }
 }
