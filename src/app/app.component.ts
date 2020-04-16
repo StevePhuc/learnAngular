0e83d0feada7a818438a931a9cb2e4ddf6f5315b
Steve Phuc <stevephuc.dev@gmail.com>
2020-04-16 10:35:25 +0300

    14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,14 +34,30 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(`
-    About to generate a password with
-    includes letter ${this.includeLetters}
-    includes numbers ${this.includeNumbers}
-    includes symbols ${this.includeSymbols}
-    `);
 
-    this.password = "My password"
+    const numbers = '1234567890'
+    const letters = 'abcdefghijklmnopqrstuvwyz'
+    const symbols = '!@#$%^&*()'
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = ''
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length)
+      generatedPassword += validChars[index]
+    }
+
+    this.password = generatedPassword
+
   }
   getName() {
     return "Phuc"
